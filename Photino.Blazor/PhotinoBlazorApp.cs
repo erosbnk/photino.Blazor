7232a782841bb85dc0b1f704a133fe261a642d9a
Denny09310 <k.denny2000@gmail.com>
2025-03-25 19:14:16 +0100

    fix: added lifetime shutdown on window closing

--- Photino.Blazor/PhotinoBlazorApp.cs
+++ Photino.Blazor/PhotinoBlazorApp.cs
@@ -4,13 +4,49 @@ namespace Photino.Blazor;
 
 public partial class PhotinoBlazorApp(IHost host)
 {
-    private readonly IHost _host = host;
+    internal IHost Host { get; } = host;
 
     public IHostEnvironment Environment => Services.GetRequiredService<IHostEnvironment>();
     public IHostApplicationLifetime Lifetime => Services.GetRequiredService<IHostApplicationLifetime>();
-    public IServiceProvider Services => _host.Services;
+    public IServiceProvider Services => Host.Services;
     public PhotinoWindow Window => Services.GetRequiredService<PhotinoWindow>();
     public PhotinoWebViewManager WindowManager => Services.GetRequiredService<PhotinoWebViewManager>();
+
+    private void ConfigureDefaults() => Window
+        .SetTitle("Photino Blazor App")
+        .SetUseOsDefaultSize(false)
+        .SetUseOsDefaultLocation(false)
+        .SetWidth(1000)
+        .SetHeight(900)
+        .SetLeft(450)
+        .SetTop(100);
+
+    private bool WindowClosingHandler(object sender, EventArgs e)
+    {
+        Lifetime.StopApplication();
+        return false;
+    }
+
+    internal void Initialize()
+    {
+        Host.Start();
+        Lifetime.ApplicationStopped.Register(Window.Close);
+
+        ConfigureDefaults();
+        Window.RegisterCustomSchemeHandler(PhotinoWebViewManager.BlazorAppScheme, HandleWebRequest);
+        Window.RegisterWindowClosingHandler(WindowClosingHandler);
+
+        var windowManager = Services.GetRequiredService<PhotinoWebViewManager>();
+        var rootComponents = Services.GetRequiredService<PhotinoRootComponentsList>();
+
+        foreach (var component in rootComponents)
+        {
+            _ = windowManager.Dispatcher.InvokeAsync(async () =>
+            {
+                await windowManager.AddRootComponentAsync(component.ComponentType, component.Selector, component.Parameters);
+            });
+        }
+    }
 
     public Stream HandleWebRequest(object? sender, string? scheme, string url, out string contentType)
         => WindowManager.HandleWebRequest(sender, scheme, url, out contentType!)!;
@@ -25,33 +61,4 @@ public partial class PhotinoBlazorApp(IHost host)
         WindowManager.Navigate(Window.StartUrl);
         Window.WaitForClose();
     }
-
-    internal void Initialize()
-    {
-        _host.Start();
-        Lifetime.ApplicationStopped.Register(Window.Close);
-
-        ConfigureDefaults();
-        Window.RegisterCustomSchemeHandler(PhotinoWebViewManager.BlazorAppScheme, HandleWebRequest);
-
-        var windowManager = Services.GetRequiredService<PhotinoWebViewManager>();
-        var rootComponents = Services.GetRequiredService<PhotinoRootComponentsList>();
-
-        foreach (var component in rootComponents)
-        {
-            _ = windowManager.Dispatcher.InvokeAsync(async () =>
-            {
-                await windowManager.AddRootComponentAsync(component.ComponentType, component.Selector, component.Parameters);
-            });
-        }
-    }
-
-    private void ConfigureDefaults() => Window
-        .SetTitle("Photino Blazor App")
-        .SetUseOsDefaultSize(false)
-        .SetUseOsDefaultLocation(false)
-        .SetWidth(1000)
-        .SetHeight(900)
-        .SetLeft(450)
-        .SetTop(100);
 }
